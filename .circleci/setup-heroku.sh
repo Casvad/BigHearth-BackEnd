582c9934e362c923477736779456607de7763720
martinjhm271 <martinjhm271@hotmail.es>
2018-02-13 14:39:12 -0500

    Intento circleci 2.0 8

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -7,11 +7,11 @@
 
   cat > ~/.netrc << EOF
   machine api.heroku.com
-    login login
-    password api
+    login $HEROKU_LOGIN
+    password $HEROKU_API_KEY
   machine git.heroku.com
-    login login
-    password api
+    login $HEROKU_LOGIN
+    password $HEROKU_API_KEY
   EOF
 
   # Add heroku.com to the list of known hosts
