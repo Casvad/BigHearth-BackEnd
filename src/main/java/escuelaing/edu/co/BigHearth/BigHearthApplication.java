8133040959f0aadd577da8f329fb9ac21d0a0c77
Carlos Alberto Ramirez Otero <carlosramirez-2997@hotmail.com>
2018-02-13 08:44:26 -0500

    Inicio proyecto

---
+++ src/main/java/escuelaing/edu/co/BigHearth/BigHearthApplication.java
@@ -0,0 +1,12 @@
+package escuelaing.edu.co.BigHearth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigHearthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigHearthApplication.class, args);
+	}
+}
