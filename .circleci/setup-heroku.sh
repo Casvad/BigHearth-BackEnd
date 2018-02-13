79aa7d0e7e145430970ee2f5091d0df9ba373657
martinjhm271 <martinjhm271@hotmail.es>
2018-02-13 15:15:54 -0500

    Update setup-heroku.sh

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-  git remote add heroku https://git.heroku.com/bighearth.git
+  git remote set-url origin git@github.com:martinjhm271/BigHearth-BackEnd.git
   wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
   sudo mkdir -p /usr/local/lib /usr/local/bin
   sudo tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
