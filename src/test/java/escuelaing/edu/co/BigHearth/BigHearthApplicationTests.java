27c3e1770ba1a8c13ddcac391ebe1dc51f861791
martinjhm271 <martinjhm271@hotmail.es>
2018-02-19 12:27:45 -0500

    Correction logic User

--- src/test/java/escuelaing/edu/co/BigHearth/BigHearthApplicationTests.java
+++ src/test/java/escuelaing/edu/co/BigHearth/BigHearthApplicationTests.java
@@ -5,11 +5,11 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringBootTest.class)
-@SpringBootTest
+//@RunWith(SpringBootTest.class)
+//@SpringBootTest
 public class BigHearthApplicationTests {
 
-	@Test
+	//@Test
 	public void contextLoads() {
 	}
 
